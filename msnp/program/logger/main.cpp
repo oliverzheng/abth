9b27c22d50a4adc91de18b9df651f512a9c1eccd
Oliver Zheng <gitcommits+laptop@oliverzheng.com>
2008-12-02 15:18:54 -0800

    CMake now works. Initial proof of concept with packetwrapper to use winpcap. Initial commit for logger to use packetwrapper.

---
+++ msnp/program/logger/main.cpp
@@ -0,0 +1,9 @@
+#include <packetwrapper/packetobserver.hpp>
+
+using namespace packetwrapper;
+
+int main()
+{
+	PacketObserver * po = new PacketObserver();
+	return 0;
+}
---
+++ msnp/program/packetwrapper/packetobserver.hpp
@@ -0,0 +1,14 @@
+#ifndef PACKET_OBSERVER_HPP
+#define PACKET_OBSERVER_HPP
+
+namespace packetwrapper {
+
+class PacketObserver
+{
+public:
+	PacketObserver();
+}; /* class PacketObserver */
+
+} /* namespace packetwrapper */
+
+#endif
