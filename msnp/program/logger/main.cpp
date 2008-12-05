688af5c654cd23d5a17cc77a61e225ce48d99007
Oliver Zheng <gitcommits+laptop@oliverzheng.com>
2008-12-04 21:12:15 -0800

    Functionality to select interface at run time in logger

--- msnp/program/logger/main.cpp
+++ msnp/program/logger/main.cpp
@@ -11,14 +11,35 @@ using namespace std;
 int main()
 {
 	try {
+		int totalInterfaces = 0;
 		list<Interface> ifs = Interface::listInterfaces();
-		for (list<Interface>::iterator it = ifs.begin(); it != ifs.end(); it++)
-			cout << (*it).name << " (" << (*it).description << ")" << endl;
+		for (list<Interface>::iterator it = ifs.begin(); it != ifs.end(); it++, totalInterfaces++) {
+			cout << totalInterfaces << ". " << (*it).name << " (" << (*it).description << ")" << endl;
+			cout << "-- addr: " << (*it).addresses.addr << endl;
+			cout << "-- netmask: " << (*it).addresses.netmask << endl;
+			cout << "-- broadaddr: " << (*it).addresses.broadaddr << endl;
+			cout << "-- dstaddr: " << (*it).addresses.dstaddr << endl;
+			cout << endl;
+		}
+
+		int whichInterface;
+
+		do {
+			cout << "Enter interface #: ";
+			cin >> whichInterface;
+			cout << endl;
+	
+			if (whichInterface >= 0 && whichInterface < totalInterfaces)
+				break;
+
+			cout << "Invalid Interface." << endl;
+		} while (true);
 
 		list<Interface>::iterator it = ifs.begin();
-		it++;
-		it++;
+		for (int i = 0; i < whichInterface; i++)
+			it++;
 		(*it).open(false);
+		cout << "Listening on " << (*it).name << endl;
 
 		PacketObserver * po = new PacketObserver();
 		PacketObservable * logger = new Logger();
