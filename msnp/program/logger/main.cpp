d4af9e802537f0f4db01a6051c9a5e8dc52ef499
Oliver Zheng <gitcommits+laptop@oliverzheng.com>
2008-12-04 21:30:05 -0800

    Cleaned up logger main.

--- msnp/program/logger/main.cpp
+++ msnp/program/logger/main.cpp
@@ -10,49 +10,53 @@ using namespace std;
 
 int main()
 {
+	list<Interface> * ifs = NULL;
+
 	try {
-		int totalInterfaces = 0;
-		list<Interface> ifs = Interface::listInterfaces();
-		for (list<Interface>::iterator it = ifs.begin(); it != ifs.end(); it++, totalInterfaces++) {
-			cout << totalInterfaces << ". " << (*it).name << " (" << (*it).description << ")" << endl;
-			cout << "-- addr: " << (*it).addresses.addr << endl;
-			cout << "-- netmask: " << (*it).addresses.netmask << endl;
-			cout << "-- broadaddr: " << (*it).addresses.broadaddr << endl;
-			cout << "-- dstaddr: " << (*it).addresses.dstaddr << endl;
-			cout << endl;
-		}
-
-		int whichInterface;
-
-		do {
-			cout << "Enter interface #: ";
-			cin >> whichInterface;
-			cout << endl;
-	
-			if (whichInterface >= 0 && whichInterface < totalInterfaces)
-				break;
-
-			cout << "Invalid Interface." << endl;
-		} while (true);
-
-		list<Interface>::iterator it = ifs.begin();
-		for (int i = 0; i < whichInterface; i++)
-			it++;
-		(*it).open(false);
-		cout << "Listening on " << (*it).name << endl;
-
-		PacketObserver * po = new PacketObserver();
-		PacketObservable * logger = new Logger();
-
-		po->setInterface(*it);
-		po->setObservable(logger);
-
-		po->start();
-		po->wait();
-
-	} catch (...) {
-		cout << "what happened here?" << endl;
+		ifs = new list<Interface>(Interface::listInterfaces());
+	} catch (ListInterfacesException & e) {
+		cerr << "Cannot list all interfaces" << endl;
+		return -1;
 	}
+
+	int totalInterfaces = 0;
+	for (list<Interface>::iterator it = ifs->begin(); it != ifs->end(); it++, totalInterfaces++) {
+		cout << totalInterfaces << ". " << (*it).name << " (" << (*it).description << ")" << endl;
+		cout << "-- addr: " << (*it).addresses.addr << endl;
+		cout << "-- netmask: " << (*it).addresses.netmask << endl;
+		cout << "-- broadaddr: " << (*it).addresses.broadaddr << endl;
+		cout << "-- dstaddr: " << (*it).addresses.dstaddr << endl;
+		cout << endl;
+	}
+
+	int whichInterface;
+
+	do {
+		cout << "Enter interface #: ";
+		cin >> whichInterface;
+
+		if (whichInterface >= 0 && whichInterface < totalInterfaces)
+			break;
+
+		cout << "Invalid Interface." << endl;
+	} while (true);
+
+	list<Interface>::iterator it = ifs->begin();
+	for (int i = 0; i < whichInterface; i++)
+		it++;
+	(*it).open(false);
+	cout << "Listening on " << (*it).name << endl;
+
+	PacketObserver * po = new PacketObserver();
+	PacketObservable * logger = new Logger();
+
+	po->setInterface(*it);
+	po->setObservable(logger);
+
+	po->start();
+	po->wait();
+
+	delete ifs;
 
 	return 0;
 }
